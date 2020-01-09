b123f9311770fda7bfe71e94478e068f5685e872
Rayque Oliveira <rayque.oliveira@gmail.com>
2020-01-09 16:51:15 -0400

    add pergunta atual

--- src/components/Pergunta/PainelGeral.vue
+++ src/components/Pergunta/PainelGeral.vue
@@ -1,114 +1,5 @@
 <template>
     <div>
-        
-        <v-row>
-            <v-col>
-                <v-card >
-                    <div class="text-center">
-                        <h1 class="pa-2 display-1">
-                            <b>Painel Geral</b>
-                        </h1>
-                        <v-divider></v-divider>
-                    </div>
-                </v-card>
-            </v-col>
-        </v-row>
-
-        <v-row>
-            <v-col>
-
-                <v-card >
-                    <div class="text-center">
-                        <h1 class="pa-2 display-1">
-                            Perguntas
-                        </h1>
-                        <v-divider></v-divider>
-                    </div>
-
-                    <v-row dense class="pa-4" v-if="!perguntaAtual">
-                        <v-col sm="12">
-                            <v-alert
-                                    color="primary"
-                                    dark
-                                    type="info"
-                                    border="left"
-                                    prominent
-                                    class="display-2"
-                            >
-                                Não há perguntas disponíveis.
-                            </v-alert>
-
-                        </v-col>
-                    </v-row>
-
-                    <v-row dense v-if="perguntaAtual" class="pa-4">
-                        <v-col cols="4">
-                            <v-card class="pa-2 grey lighten-2 grey--text headline text-center" tile>
-                                <div class="mb-2">
-                                    <span>Anterior</span>
-                                </div>
-                                <div class="mb-2">
-                                    <span> {{ perguntaAnterior }} </span>
-                                </div>
-                            </v-card>
-                        </v-col>
-                        <v-col cols="4">
-                            <v-card class="pa-2 display-1" tile>
-                                <div class="mb-2 text-center">
-                                    <span >Atual</span>
-                                </div>
-                                <div class="mb-2 text-center">
-                                    <span> {{ perguntaAtual }} </span>
-                                </div>
-                            </v-card>
-                        </v-col>
-                        <v-col cols="4">
-                            <v-card class="pa-2 grey lighten-2 grey--text headline text-center" tile>
-                                <div class="mb-2 ">
-                                    <span>Próxima</span>
-                                </div>
-                                <div class="mb-2">
-                                    <span> {{ proximaPergunta }} </span>
-                                </div>
-                            </v-card>
-                        </v-col>
-                    </v-row>
-
-                    <v-divider></v-divider>
-
-                </v-card>
-            </v-col>
-        </v-row>
-
-        <v-row v-if="perguntaAtual">
-
-            <v-col>
-
-                <v-card >
-<!--                    <div class="text-center">-->
-<!--                        <h1 class="pa-2 display-1">-->
-<!--                            Pontuação das Equipes-->
-<!--                        </h1>-->
-<!--                        <v-divider></v-divider>-->
-<!--                    </div>-->
-
-<!--                    <v-divider></v-divider>-->
-
-<!--                    <v-row no-gutters  >-->
-<!--                        <v-col md="4" v-for="equipe in pontuacao">-->
-<!--                            <v-row no-gutters justify="center" class="title" :class="getColor(equipe.pontuacao)"  >-->
-<!--                                <v-col  md="8"> {{ equipe.nome }} </v-col>-->
-<!--                                <v-col md="2"> {{ equipe.pontuacao }} </v-col>-->
-<!--                            </v-row>-->
-<!--                        </v-col>-->
-<!--                    </v-row>-->
-
-
-
-
-                </v-card>
-            </v-col>
-        </v-row>
 
 
 
@@ -130,8 +21,88 @@
                     </v-toolbar>
 
 
+                    <v-row>
+                        <v-col>
+                            <v-card >
+                                <div class="text-center">
+                                    <h1 class="pa-2 display-1">
+                                        <b>Painel Geral</b>
+                                    </h1>
+                                    <v-divider></v-divider>
+                                </div>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+
+                    <v-row>
+                        <v-col>
+
+                            <v-card >
+                                <div class="text-center">
+                                    <h1 class="pa-2 headline">
+                                        Perguntas
+                                    </h1>
+                                    <v-divider></v-divider>
+                                </div>
+
+                                <v-row dense class="pa-4" v-if="!perguntaAtual">
+                                    <v-col sm="12">
+                                        <v-alert
+                                                color="primary"
+                                                dark
+                                                type="info"
+                                                border="left"
+                                                prominent
+                                                class="display-2"
+                                        >
+                                            Não há perguntas disponíveis.
+                                        </v-alert>
+
+                                    </v-col>
+                                </v-row>
+
+                                <v-row dense v-if="perguntaAtual">
+                                    <v-col cols="4">
+                                        <v-card class="pa-2 grey lighten-2 grey--text subtitle-1 text-center" tile>
+                                            <div class="mb-2">
+                                                <span>Anterior</span>
+                                            </div>
+                                            <div class="mb-2">
+                                                <span> {{ perguntaAnterior }} </span>
+                                            </div>
+                                        </v-card>
+                                    </v-col>
+                                    <v-col cols="4">
+                                        <v-card class="pa-2 headline" tile>
+                                            <div class="mb-2 text-center">
+                                                <span >Atual</span>
+                                            </div>
+                                            <div class="mb-2 text-center">
+                                                <span> {{ perguntaAtual }} </span>
+                                            </div>
+                                        </v-card>
+                                    </v-col>
+                                    <v-col cols="4">
+                                        <v-card class="pa-2 grey lighten-2 grey--text subtitle-1 text-center" tile>
+                                            <div class="mb-2 ">
+                                                <span>Próxima</span>
+                                            </div>
+                                            <div class="mb-2">
+                                                <span> {{ proximaPergunta }} </span>
+                                            </div>
+                                        </v-card>
+                                    </v-col>
+                                </v-row>
+
+                                <v-divider></v-divider>
+
+                            </v-card>
+                        </v-col>
+                    </v-row>
+
 
                     <v-row v-if="perguntaAtual">
+
 
                         <v-col>
                             <v-card>
